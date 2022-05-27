fabba19cb5747dc628c05cb49cbcb58362acce6d
sirgak <life_4_ever@mail.ru>
2022-05-27 11:04:48 +0300

    text
    t

---
+++ homework007/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homework007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTime;
+            string text;
+            Console.Write("Введите текст: ");
+            text = Convert.ToString (Console.ReadLine());
+            Console.Write("Введите количество повторов текста: ");
+            numberOfTime = Convert.ToInt32(Console.ReadLine());
+            while (numberOfTime > 0)
+            {
+                Console.WriteLine(text);
+                numberOfTime--;
+            }
+        }
+    }
+}
